b0568cad3eeed400af5d7a97ca163d945f9a1ec4
Nezzerath <larry.dixon95@gmail.com>
2023-01-20 08:33:55 -0600

    Initial commit

---
+++ Content/Items/IronSkin.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace Enchanter.Content.Items
+{
+    public class IronSkin : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("IronSkin Buff");
+            Tooltip.SetDefault("Equip to get ironskin buff");
+        }
+
+    }
+}
---
+++ Enchanter.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace Enchanter
+{
+	public class Enchanter : Mod
+	{
+	}
+}
